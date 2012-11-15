af7f6b9be922efa9c17508d557d3af4a6919f8c1
Wuchao <ariesnix93@gmail.com>
2012-11-15 15:11:08 +0800

    Count Address

--- AnalysisAddress/src/classify/TypeAnalysis.java
+++ AnalysisAddress/src/classify/TypeAnalysis.java
@@ -1,6 +1,9 @@
 package classify;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.HttpURLConnection;
@@ -12,8 +15,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
+import net.sf.json.JSONObject;
 
 public class TypeAnalysis {
 
@@ -22,7 +24,6 @@ public class TypeAnalysis {
 	 */
 	private double[] lat = new double[2000];
 	private double[] lng = new double[2000];
-	private int[][] count = new int[20][2000];
 
 	private final static String[] aeraSpecies = {
 		"美食","休闲娱乐","购物","结婚","宾馆酒店","旅游景点",
@@ -33,24 +34,37 @@ public class TypeAnalysis {
 		// TODO Auto-generated method stub
 
 		TypeAnalysis ob = new TypeAnalysis();
-		String outputAddr = "E://AeraCount//AeraCount.txt";
-		ob.getLatLng();
+		String outputAddr = "E://AeraCount//AeraCount2.txt";
+		int n = ob.getLatLng();
 		try {
+			PrintWriter output = new PrintWriter(
+					new BufferedWriter(new FileWriter(outputAddr,false)));
+			for (int i = 0; i < n; i++) {
+				output.println(ob.lat[i] + "\t" + ob.lng[i]);
+			}
+			output.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	/*	try {
 			String city = URLEncoder.encode("呼和浩特", "UTF-8");
-			PrintWriter out = new PrintWriter(outputAddr);
+			PrintWriter output = new PrintWriter(
+					new BufferedWriter(new FileWriter(outputAddr,false)));
+			output.println("\t经度\t纬度\t总数");
 			for (int i = 0; i < ob.lat.length; i++) {
-				System.out.println(ob.lat[i] + "*" + ob.lng[i]);
-				for (int j = 0; j < aeraSpecies.length; j++) {
-					// JSONObject json_result = new JSONObject();
-					// JSONArray json_list = new JSONArray();
+				output.print(i + "\t" + ob.lat[i] + "\t" + ob.lng[i]);
+				System.out.print(i + "\t" + ob.lat[i] + "\t" + ob.lng[i]);
+//				for (int j = 0; j < aeraSpecies.length; j++) {
+					JSONObject json_result = new JSONObject();
 					String str = "http://openapi.aibang.com/search" +
 							"?app_key=f41c8afccc586de03a99c86097e98ccb" +
 							"&city=" + city +
 							"&lat=" + ob.lat[i] + "&lng=" + ob.lng[i] +
-							"&q=" + URLEncoder.encode(
-									aeraSpecies[j],"UTF-8") +
-							"&as=2500&alt=json&from=1&to=300";
-					System.out.println(str);
+//							"&q=" + URLEncoder.encode(
+//									aeraSpecies[j],"UTF-8") +
+							"&as=1000&alt=json&from=1&to=300";
+					System.out.print("\t" + str);
 					URL url = new URL(str);
 					HttpURLConnection connection =
 							(HttpURLConnection)url.openConnection();
@@ -59,37 +73,40 @@ public class TypeAnalysis {
 							new InputStreamReader(
 									connection.getInputStream(),"UTF-8"));
 					String line = reader.readLine();
-					// json_list = JSONArray.fromObject(line);
-					out.println(i + ":" + line);
+					json_result = JSONObject.fromObject(line);
+					int count = json_result.getInt("index_num");
+					output.println("\t" + count);
+					System.out.println("\t" + count);
 					reader.close();
 					connection.disconnect();
-				}
+//				}
 			}
-			out.close();
+			output.close();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		}*/
 	}
 
-	private void getLatLng() {
+	private int getLatLng() {
 		//得到经纬度数据
 		Connection conn = null;
 		Statement stmt = null;
+		int i = 0;
 		try{
 //			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 			String query = "Select Latitude,Longitude " +
-					"From ZhuData.dbo.new_SiteInfo";
+					"From ZhuData.dbo.new_SiteInfo_2 " +
+					"Order By UserCount desc";
 			conn = DriverManager.getConnection(
 					"jdbc:sqlserver://localhost:1433;" +
 					"databaseName=ZhuData;" +
 					"integratedSecurity=true;");
 			stmt = conn.createStatement();
 			ResultSet rs = stmt.executeQuery(query);
-			int i = 0;
 			while(rs.next()) {
 				lat[i] = rs.getDouble("Latitude");
-				lng[i]= rs.getDouble("Longitude");
+				lng[i] = rs.getDouble("Longitude");
 				i++;
 			}
 			stmt.close();
@@ -97,6 +114,7 @@ public class TypeAnalysis {
 		}catch(SQLException e) {
 			e.printStackTrace();
 		}
+		return i;
 	}
 
 }
