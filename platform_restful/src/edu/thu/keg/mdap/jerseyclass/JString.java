22224e041a9b7c1e2e2f32b3b6bedcc4bf72986e
Yuan Bozhi <lawby1229@163.com>
2013-04-03 23:46:36 +0800

    recoded  dataset function
    used jersey's functions

---
+++ platform_restful/src/edu/thu/keg/mdap/jerseyclass/JString.java
@@ -0,0 +1,38 @@
+package edu.thu.keg.mdap.jerseyclass;
+
+import java.lang.String;
+
+public class JString {
+	private String classname;
+	private double longitude;
+	private double latitude;
+
+	public void inputLocation(double longitude, double latitude) {
+		this.longitude = longitude;
+		this.latitude = latitude;
+	}
+
+	public void setClassname(String classname) {
+		this.classname = classname;
+	}
+
+	public String getClassname() {
+		return this.classname;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+
+	public double getLongitude() {
+		return this.longitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public double getLatitude() {
+		return this.latitude;
+	}
+}
--- platform_restful/src/edu/thu/keg/mdap/restful/DataSetFunctions.java
+++ platform_restful/src/edu/thu/keg/mdap/restful/DataSetFunctions.java
@@ -1,43 +1,32 @@
 package edu.thu.keg.mdap.restful;
 
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ResourceBundle;
 
-import javax.naming.OperationNotSupportedException;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import com.sun.jersey.core.impl.provider.header.MediaTypeProvider;
-
 import edu.thu.keg.mdap.DataSetManager;
 import edu.thu.keg.mdap.Platform;
 import edu.thu.keg.mdap.dataset.DataSet;
-import edu.thu.keg.mdap.provider.DataProviderException;
-import edu.thu.keg.mdap.provider.IllegalQueryException;
-import edu.thu.keg.mdap_impl.PlatformImpl;
-
-import net.sf.json.JSONArray;
-import net.sf.json.JSONObject;
-import net.sf.json.JSONSerializer;
-import net.sf.json.JsonConfig;
-import net.sf.json.xml.XMLSerializer;
+import edu.thu.keg.mdap.jerseyclass.JString;
 
 @Path("/dataset")
 public class DataSetFunctions {
-	// This method is called if TEXT_PLAIN is request
+
 	@GET
 	@Path("/getdatasets")
-	@Produces(MediaType.APPLICATION_JSON)
-	public String getAllDataSetsNames_json() {
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public List<JString> getAllDataSetsNames_list() {
 		String result = "";
+		List<JString> datasetsName = new ArrayList<JString>();
 		try {
 			String PI = ResourceBundle.getBundle("platform_initial").getString(
 					"PlatformImpl");
@@ -47,74 +36,27 @@ public class DataSetFunctions {
 
 			Platform DMSIInstance = (Platform) PIClass.newInstance();
 			DataSetManager datasetManager = DMSIInstance.getDataSetManager();
-			ArrayList<String> datasetsName = new ArrayList<>();
 			Collection<DataSet> datasets = datasetManager.getDataSetList();
 			Iterator<DataSet> dataset_it = datasets.iterator();
 			while (dataset_it.hasNext()) {
-				datasetsName.add(dataset_it.next().getName());
+				JString dname = new JString();
+				datasetsName.add(dname);
 			}
-			// int i=0;
-			// while(i<10)
-			// {
-			// datasetsName.add(String.valueOf(i));
-			// i++;
-			// }
-			JSONArray jsonArray = (JSONArray) JSONSerializer
-					.toJSON(datasetsName);
-			result = jsonArray.toString();
-		}
 
-		catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InstantiationException e) {
-			// TODO Auto-generated catch block
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return result;
-	}
-
-	@GET
-	@Path("/getdatasets")
-	@Produces(MediaType.APPLICATION_XML)
-	public String getAllDataSetsNames_xml() {
-		String result = "";
-		try {
-			String PI = ResourceBundle.getBundle("platform_initial").getString(
-					"PlatformImpl");
-			Class PIClass = Class.forName(PI);
-			Platform DMSIInstance = (Platform) PIClass.newInstance();
-			DataSetManager datasetManager = DMSIInstance.getDataSetManager();
-			ArrayList<String> datasetsName = new ArrayList<>();
-			Collection<DataSet> datasets = datasetManager.getDataSetList();
-			Iterator<DataSet> dataset_it = datasets.iterator();
-			while (dataset_it.hasNext()) {
-				datasetsName.add(dataset_it.next().getName());
-			}
-			XMLSerializer xmlSerializer = new XMLSerializer();
-			result = xmlSerializer.write(JSONSerializer.toJSON(datasetsName));
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InstantiationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return result;
+		return datasetsName;
 	}
 
 	@GET
 	@Path("/getdatasetlocation/{dataasetname}")
-	@Produces(MediaType.APPLICATION_JSON)
-	public String getDataSetLocation_json(
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public List<JString> getDataSetLocation_list(
 			@PathParam("datasetname") String datasetname) {
 		String result = "";
+
+		List<JString> al_rs = new ArrayList<JString>();
 		try {
 			String PI = ResourceBundle.getBundle("platform_initial").getString(
 					"PlatformImpl");
@@ -123,78 +65,26 @@ public class DataSetFunctions {
 			DataSetManager datasetManager = DMSIInstance.getDataSetManager();
 			DataSet dataset = datasetManager.getDataSet(datasetname);
 			ResultSet rs = dataset.getResultSet();
-			ArrayList<String> al_rs = new ArrayList<>();
-			String line = "";
 			while (rs.next()) {
-				al_rs.add("[" + rs.getString("Longitude") + ","
-						+ rs.getString("Latitude") + "]");
+				JString location = new JString();
+				location.inputLocation(rs.getDouble("Longitude"),
+						rs.getDouble("Latitude"));
+				al_rs.add(location);
 			}
-			JSONArray jsonArray = (JSONArray) JSONSerializer.toJSON(al_rs);
-			result = jsonArray.toString();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InstantiationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (OperationNotSupportedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (DataProviderException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return result;
+		return al_rs;
 	}
-	
-	@GET
-	@Path("/getdatasetlocation/{dataasetname}")
-	@Produces(MediaType.APPLICATION_XML)
-	public String getDataSetLocation_xml(
-			@PathParam("datasetname") String datasetname) {
-		String result = "";
-		try {
-			String PI = ResourceBundle.getBundle("platform_initial").getString(
-					"PlatformImpl");
-			Class PIClass = Class.forName(PI);
-			Platform DMSIInstance = (Platform) PIClass.newInstance();
-			DataSetManager datasetManager = DMSIInstance.getDataSetManager();
-			DataSet dataset = datasetManager.getDataSet(datasetname);
-			ResultSet rs = dataset.getResultSet();
-			ArrayList<String> al_rs = new ArrayList<>();
-			String line = "";
-			while (rs.next()) {
-				al_rs.add("[" + rs.getString("Longitude") + ","
-						+ rs.getString("Latitude") + "]");
-			}
-			XMLSerializer xmlSerializer = new XMLSerializer();
-			result = xmlSerializer.write(JSONSerializer.toJSON(al_rs));
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InstantiationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (OperationNotSupportedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (DataProviderException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return result;
-	}
+
+	// public String getDataSetColumn_json() {
+	// String result = "";
+	// return result;
+	// }
+	//
+	// public String getDataSetColumn_xml() {
+	// String result = "";
+	// return result;
+	// }
 
 }
