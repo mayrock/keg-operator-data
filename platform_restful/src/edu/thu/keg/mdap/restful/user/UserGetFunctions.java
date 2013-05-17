abb16c06d7fd8981609d0f57de81d3e7923ceeb8
Yuan Bozhi <lawby1229@163.com>
2013-05-17 19:14:35 +0800

    add tfor user

--- platform_restful/src/edu/thu/keg/mdap/restful/user/UserGetFunctions.java
+++ platform_restful/src/edu/thu/keg/mdap/restful/user/UserGetFunctions.java
@@ -1,10 +1,27 @@
 package edu.thu.keg.mdap.restful.user;
 
+import java.util.List;
+
 import javax.servlet.ServletContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
+
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+
+import com.sun.jersey.api.json.JSONWithPadding;
 
 @Path("/ug")
 public class UserGetFunctions {
@@ -14,4 +31,42 @@ public class UserGetFunctions {
 	Request request;
 	@Context
 	ServletContext servletcontext;
+
+	@GET
+	@Path("/verifyuser/{userid}/{pw}")
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	public JSONWithPadding verifyUser(
+			@QueryParam("jsoncallback") @DefaultValue("fn") String callback) {
+		JSONObject job = null;
+		try {
+			job = new JSONObject();
+			job.append("status", true);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.print(job.toString());
+		// return Response.created(uriInfo.getAbsolutePath()).build();
+		return new JSONWithPadding(new GenericEntity<String>(job.toString()) {
+		}, callback);
+	}
+
+	@GET
+	@Path("/adduser")
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	public JSONWithPadding addUser(
+			@QueryParam("jsoncallback") @DefaultValue("fn") String callback) {
+		JSONObject job = null;
+		try {
+			job = new JSONObject();
+			job.append("status", true);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.print(job.toString());
+		// return Response.created(uriInfo.getAbsolutePath()).build();
+		return new JSONWithPadding(new GenericEntity<String>(job.toString()) {
+		}, callback);
+	}
 }
